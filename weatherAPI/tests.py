736c5f395575a5411caa9c9e6e9e32aee7da1b2f
Norberto Moreno <norbermv.dev@gmail.com>
2021-08-22 16:51:56 -0500

    Commenting code.

--- weatherAPI/tests.py
+++ weatherAPI/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+""" Weather API ."""
+
+# Django rest framework
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+# Utils
+from . requestapi import get_weather
+
+
 
 # Create your tests here.
+
+# Live test
+class WeatherAPITest(APITestCase):
+    """ Run test over the weather API to check
+        the status."""
+    def test_api_url(self):
+        
+        url = 'http://localhost:8000/weather?city=Bogota&country=co&'  #
+        response = self.client.get(url)
+        self.assertTrue(status.is_success(response.status_code))
+        
+        
+
+
+class FuncThirdAPITest(APITestCase):
+    """ Run test over the test_get_weather() function
+        from the requestapi.py module."""
+
+    def test_get_weather(self):
+        # Testing variables
+        city = 'Medellin'
+        country = 'co'
+
+        weather_data = get_weather(city, country)         # Call to the function
+        
+        self.assertEqual(weather_data['location_name'], 'Medellín, CO') 
+
+
