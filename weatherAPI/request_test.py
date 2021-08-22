70d5173bd406654dade061344ce6950b4ddd16f3
Norberto Moreno <norbermv.dev@gmail.com>
2021-08-22 11:50:00 -0500

    Retrive data in XML, from the API using xmltodict module.

--- weatherAPI/request_test.py
+++ weatherAPI/request_test.py
@@ -3,35 +3,61 @@ import os
 import requests
 import pdb
 from datetime import datetime
+import xmltodict
+import json
+#import xml.etree.ElementTree as ET
 
 
 def get_weather(city, country):
 
-    api_key = os.environ.get('WEATHER_SECRET_APIKEY')
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}"
-
+    api_key =  os.environ.get('WEATHER_SECRET_APIKEY')
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&mode=xml"
+    #pdb.set_trace()
     response = requests.get(url) #.json()
-    data = response.json()       # Return all weather data in JSON
+          
+    #root = ET.fromstring(response.content)
+    
+    data_json = json.dumps(xmltodict.parse(response.content)) # JSON string
+    #print(root)
 
+    current_data = json.loads(data_json)       # Return all weather data in JSON ALL GOOD SO FAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    data = current_data['current']
     # Kelvin to Celsius formula: °K − 273.15 = °C
     # Fields units API response:  https://openweathermap.org/current#parameter
     
-    
-    #city = data['name']
-    #country = data['sys']['country']
-    location_name = "{},{}".format(data['name'], data['sys']['country'])
-    tempc = data['main']['temp'] - 273.15
+    #pdb.set_trace()
+    #city = data['city']['@name']
+    #country = data['city']['country']   
+    location_name = "{}, {}".format(data['city']['@name'], data['city']['country'] )
+    tempc = float(data['temperature']['@value']) - 273.15
     tempf = (tempc * (9/5)) + 32
-    pressure = data['main']['pressure']
-    humidity = data['main']['humidity']
+    # Wind variables
+    wind_name = data['wind']['speed']['@name']  # "Minor" if age < 18 else "Adult"
+    wind_descript_name = wind_name if wind_name != None else "No description available"
+    wind_val = data['wind']['speed']['@value']
+    wind_value = wind_val if wind_val != None else "No description available"
+    wind_dir = data['wind']['direction']['@name']
+    wind_direction = wind_dir if wind_dir != None else "No description available"
+
+    cloudiness = data['clouds']['@name']
+    pressure = data['pressure']['@value']
+    humidity = data['humidity']['@value']
     #sunrise_val = data['sys']['sunrise'] # unix
-    sunrise = datetime.fromtimestamp(int(data['sys']['sunrise'])).strftime('%H:%M hrs')
-    sunset = datetime.fromtimestamp(int(data['sys']['sunset'])).strftime('%H:%M hrs')
-    coord_lat = data['coord']['lat']
-    coord_lon = data['coord']['lon']
-    dt = data['dt'] # UNIX Timestamp
-    requested_time = datetime.fromtimestamp(int(dt)).strftime('%b %dth, %Y - %H:%M:%S    hrs')
+    
+    sunrise_list = data['city']['sun']['@rise'].split("T")
+    sunrise = sunrise_list[1]
+    # datetime.fromtimestamp(int(data['city']['sun']['@rise'])).strftime('%H:%M hrs')
+    sunset_list = data['city']['sun']['@set'].split("T") # datetime.fromtimestamp(int(data['city']['sun']['@set'])).strftime('%H:%M hrs')
+    sunset = sunset_list[1]
+    coord_lat = data['city']['coord']['@lat']
+    coord_lon = data['city']['coord']['@lon']
+    #dt = data['dt'] # UNIX Timestamp
+    requested_time = data['lastupdate']['@value'] # datetime.fromtimestamp(int(dt)).strftime('%b %dth, %Y - %H:%M:%S    hrs')
     # .format(now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+
+    # Weather Conditions
+
+    
     
 
 
@@ -39,14 +65,15 @@ def get_weather(city, country):
 
         'location_name': location_name,
         'temperature': '%.2f°C, %.2f°F' % (tempc,tempf), #'{}%.2f °C , {} °F'.format(tempc, tempf),
-        'wind': None,                                    # Gentle breeze, 3.6 m/s, west-northwest",
-        'cloudiness': None,                              # "Scattered clouds",
+        'wind': '{}, {} m/s, {}'.format(wind_descript_name, wind_value, wind_direction),                                    # Gentle breeze, 3.6 m/s, west-northwest",
+        'cloudiness': cloudiness,                              # "Scattered clouds",
         'pressure': '{} hpa'.format(pressure),
         'humidity': '{} %'.format(humidity),
         'sunrise': sunrise, 
         'sunset': sunset,
         'geo_coordinates': '[{}, {}]'.format(coord_lat, coord_lon),
         'requested_time': requested_time,
+        'forecast': '{...}'
 
     }
 
